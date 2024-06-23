74bfaab08df4101742e58cc3e8e6ff3df5cd0c71
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-06-23 17:10:24 +0200

    Card Custom Component + Adding User Component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import {CardComponent} from "../shared/card/card.component";
+import {User} from "../model/user";
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
